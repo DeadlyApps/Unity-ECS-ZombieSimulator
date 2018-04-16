f16f5ce6c858eecf26ece06b08bd8b3edd97da74
Christopher Lucian <Chris@deadlyapps.com>
2018-04-15 20:14:09 -0700

    Initial Commit Archetypes

---
+++ ECS Zombie Simulator/Assets/GameCode/ZombieSimulatorBootstrap.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Transforms2D;
+using UnityEngine;
+
+
+public class ZombieSimulatorBootstrap {
+
+    public static EntityArchetype HumanArchetype { get; private set; }
+    public static EntityArchetype ZombieArchetype { get; private set; }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void Initialize()
+    {
+        var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+
+        DefineArchetypes(entityManager);
+
+    }
+
+    private static void DefineArchetypes(EntityManager entityManager)
+    {
+        //Humans - Composition of the humans
+
+        HumanArchetype = entityManager.CreateArchetype(typeof(Position2D), typeof(TransformMatrix));
+
+        //Zombies
+
+        ZombieArchetype = entityManager.CreateArchetype(typeof(Position2D), typeof(TransformMatrix));
+    }
+}
